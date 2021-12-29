24eedc134ed2c20026a8814c63c7fd7b274be862
Joseph Sungpil Choi <chousemath@gmail.com>
2021-12-29 19:24:20 +0900

    feat: Add Tierra's User code to main.py

--- main.py
+++ main.py
@@ -19,6 +19,19 @@ db = client.wcoding
 app = FastAPI()
 
 
+class User(BaseModel):
+    name: str
+    birthdate: Optional[str]
+    profile_image_url: Optional[str]
+    username: str
+    website: Optional[str]
+    pronouns: Optional[list[str]]
+    biography: Optional[str]
+    email: Optional[str]
+    phone: Optional[int]
+    gender: Optional[str]
+
+
 class Product(BaseModel):
     name: str
     price: int
@@ -157,3 +170,38 @@ async def update_post(_id: str, post: Post):
 def delete_post(_id: str):
     db.posts.delete_one({"_id": ObjectId(_id)})
     return {"ok": True}
+
+
+@app.post("/users")
+async def create_new_user(user: User):
+    db.users.insert_one(user.dict())
+    return {"message": "A new user was saved to the database"}
+
+
+@app.put("/users/{_id}")
+async def update_user(_id: str, user: User):
+    db.users.update_one({"_id": ObjectId(_id)}, {"$set": user.dict()})
+    return {"updated": True}
+
+
+@app.get("/users")
+def get_all_users():
+    users = db.users.find({})
+    user_list = []
+    for user in users:
+        user["_id"] = str(user["_id"])
+        user_list.append(user)
+    return {"users": user_list}
+
+
+@app.get("/users/{_id}")
+def find_single_user(_id: str):
+    user = db.users.find_one({"_id": ObjectId(_id)})
+    user["_id"] = str(user["_id"])
+    return user
+
+
+@app.delete("/users/{_id}")
+def delete_users(_id: str):
+    db.users.delete_one({"_id": ObjectId(_id)})
+    return {"deleted": True}

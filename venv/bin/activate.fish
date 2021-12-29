1ddc0c7adbffbb8b026085f7895146b950e603e8
Joseph Sungpil Choi <chousemath@gmail.com>
2021-12-29 19:23:13 +0900

    feat: Merge in Tierra's User related code

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/tthomp/Desktop/instagram-clone/venv"
+set -gx VIRTUAL_ENV "/Users/j/Desktop/projects/instagram-clone/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH

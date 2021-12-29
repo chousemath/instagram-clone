0f9a046c08bd9a591087449b0fdeca0e737ec37c
Joseph Sungpil Choi <chousemath@gmail.com>
2021-12-29 19:16:18 +0900

    feat: Merge in Julie's comment code

--- main.py
+++ main.py
@@ -7,56 +7,106 @@ from os import environ
 import certifi
 from pprint import pprint
 from bson.objectid import ObjectId
+
 load_dotenv()
 
-key = environ ['MONGODB_CONNECTION_STRING']
+key = environ["MONGODB_CONNECTION_STRING"]
 
 client = MongoClient(key, tlsCAFile=certifi.where())
 db = client.wcoding
+
 
 class Product(BaseModel):
     name: str
     price: int
 
+
 app = FastAPI()
+
+
+class Comment(BaseModel):
+    creator: str
+    body: str
+    created_at: datetime
+    updated_at: datetime
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.get('/products')
+
+@app.get("/products")
 def get_products():
-    '''
+    """
     Should fetch all products from the database
-    '''
+    """
     products = db.products.find({})
     product_list = []
     for product in products:
-        product['_id'] = str(product['_id'])
+        product["_id"] = str(product["_id"])
         product_list.append(product)
-    return {'products': product_list}
+    return {"products": product_list}
 
-@app.get('/products/{_id}')
+
+@app.get("/products/{_id}")
 def get_product(_id: str):
-    product = db.products.find_one({'_id': ObjectId(_id)}) # None
-    product['_id'] = str(product['_id'])
+    product = db.products.find_one({"_id": ObjectId(_id)})  # None
+    product["_id"] = str(product["_id"])
     return product
+
 
 @app.post("/products")
 async def create_product(product: Product):
     db.products.insert_one(product.dict())
-    return {'ok': True}
+    return {"ok": True}
 
-@app.put('/products/{_id}')
+
+@app.put("/products/{_id}")
 async def update_product(_id: str, product: Product):
-    db.products.update_one(
-        {'_id': ObjectId(_id)},
-        {'$set': product.dict()}
-    )
-    return {'ok': True}
+    db.products.update_one({"_id": ObjectId(_id)}, {"$set": product.dict()})
+    return {"ok": True}
 
-@app.delete('/products/{_id}')
+
+@app.delete("/products/{_id}")
 def delete_product(_id: str):
-    db.products.delete_one({'_id': ObjectId(_id)})
-    return {'ok': True}
+    db.products.delete_one({"_id": ObjectId(_id)})
+    return {"ok": True}
+
+
+@app.get("/comments")
+def get_comments():
+    """
+    Should fetch all comments from the database
+    """
+    comments = db.comments.find({})
+    comment_list = []
+    for comment in comments:
+        comment["_id"] = str(comment["_id"])
+        comment_list.append(comment)
+    return {"comments": comment_list}
+
+
+@app.get("/comments/{_id}")
+def get_comment(_id: str):
+    comment = db.comments.find_one({"_id": ObjectId(_id)})  # None
+    comment["_id"] = str(comment["_id"])
+    return comment
+
+
+@app.post("/comments")
+async def create_comment(comment: Comment):
+    db.comments.insert_one(comment.dict())
+    return {"ok": True}
+
+
+@app.put("/comments/{_id}")
+async def update_comment(_id: str, comment: Comment):
+    db.comments.update_one({"_id": ObjectId(_id)}, {"$set": comment.dict()})
+    return {"ok": True}
+
+
+@app.delete("/comments/{_id}")
+def delete_comment(_id: str):
+    db.comments.delete_one({"_id": ObjectId(_id)})
+    return {"ok": True}

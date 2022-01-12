c1f6a750e38445230e3838d0f6fcc0e969db3fda
Joseph Sungpil Choi <chousemath@gmail.com>
2022-01-12 20:47:32 +0900

    feat: Add two examples of using pytest

---
+++ test_products.py
@@ -0,0 +1,15 @@
+import requests
+
+
+def test_get_all_products():
+    """
+    This test should make sure that I am able
+    to fetch the full list of product data
+    from MongoDB
+    """
+    res = requests.get("http://127.0.0.1:8000/products")
+    data = res.json()
+    assert "products" in data
+    assert len(data["products"]) >= 1
+    for product in data["products"]:
+        assert "name" in product
---
+++ test_users.py
@@ -0,0 +1,11 @@
+import requests
+
+
+def test_get_all_users():
+    res = requests.get("http://127.0.0.1:8000/users")
+    data = res.json()
+    assert "users" in data
+    assert data["users"]
+    for user in data["users"]:
+        assert "username" in user
+        assert "name" in user
